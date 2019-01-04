b7d590a4339476e5ef6643520f905044497995ff
Richard Wardin <shalmezad@gmail.com>
2019-01-04 11:16:40 -0500

    Fixed styles to match pep 8

--- geneprog/api.py
+++ geneprog/api.py
@@ -1,12 +1,13 @@
 import ctypes
 import ctypes.util
+from geneprog.cdefs import (cgp_data)
+
 
 def load_library():
-	libgp_path = ctypes.util.find_library('libgeneprog.0.dylib')
-	libgp = ctypes.CDLL(str(libgp_path))
-	return libgp
+    libgp_path = ctypes.util.find_library('libgeneprog.0.dylib')
+    libgp = ctypes.CDLL(str(libgp_path))
+    return libgp
+
 
 library = load_library()
-
-from geneprog.cdefs import (cgp_data)
 cgp_data.load(library)
--- geneprog/cgpgene.py
+++ geneprog/cgpgene.py
@@ -1,22 +1,25 @@
 from .api import library
 import ctypes
 
-class CGPGene():
-	def __init__(self, num_in, num_mid, num_out):
-		print("Creating CGPGene")
-		self.gene = library.GP_CGP_alloc(num_in, num_mid, num_out).contents
-		# Sould also randomize it:
-		print("Randomizing CGPGene")
-		library.GP_CGP_randomize(self.gene)
 
-	def evaluate(self, inputs):
-		print("Evaluating")
-		print(self.gene)
-		print(self.gene.evaluate)
-		outputs = [0,0,0]
-		inputs_arr = (ctypes.c_double * len(inputs))(*inputs)
-		outputs_arr = (ctypes.c_double * len(outputs))(*outputs)
-		data = self.gene.data
-		self.gene.evaluate(inputs_arr, outputs_arr, data)
-		new_outs = [outputs_arr[i] for i in range(len(outputs))]
-		return new_outs
+class CGPGene():
+    """A simple cartesian GP gene"""
+
+    def __init__(self, num_in, num_mid, num_out):
+        print("Creating CGPGene")
+        self.gene = library.GP_CGP_alloc(num_in, num_mid, num_out).contents
+        # Sould also randomize it:
+        print("Randomizing CGPGene")
+        library.GP_CGP_randomize(self.gene)
+
+    def evaluate(self, inputs):
+        print("Evaluating")
+        print(self.gene)
+        print(self.gene.evaluate)
+        outputs = [0, 0, 0]
+        inputs_arr = (ctypes.c_double * len(inputs))(*inputs)
+        outputs_arr = (ctypes.c_double * len(outputs))(*outputs)
+        data = self.gene.data
+        self.gene.evaluate(inputs_arr, outputs_arr, data)
+        new_outs = [outputs_arr[i] for i in range(len(outputs))]
+        return new_outs
